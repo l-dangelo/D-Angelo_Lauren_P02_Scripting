a81f763ac2e651800284635d4c3c92236e10d775
Lauren D'Angelo <lkd180001@utdallas.edu>
2020-10-17 18:33:53 -0500

    last weeks homework

---
+++ Assets/Scripts/Level01/FireWeapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+    [SerializeField] Camera _playCamera = null;
+    [SerializeField] Transform rayOrigin = null;
+    [SerializeField] float _shootDistance = 10;
+    [SerializeField] GameObject _hitFeedback = null;
+    [SerializeField] float _weaponDamage = 20;
+    [SerializeField] LayerMask _hitLayers;
+
+
+    RaycastHit objectHit;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 rayDirect = _playCamera.transform.forward;
+
+        Debug.DrawRay(rayOrigin.position, rayDirect * _shootDistance, Color.red, 1f);
+
+        if(Physics.Raycast(rayOrigin.position, rayDirect, out objectHit, _shootDistance, _hitLayers))
+        {
+            if (objectHit.transform.tag.Equals("Enemy"))
+            {
+                Debug.Log("Hit " + objectHit.transform.name);
+                _hitFeedback.transform.position = objectHit.point;
+
+                enemyDamage shooter = objectHit.transform.gameObject.GetComponent<enemyDamage>();
+                if (shooter != null)
+                {
+                    shooter.TakeDamage(_weaponDamage);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Miss");
+        }
+    }
+}
